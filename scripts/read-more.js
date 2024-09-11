a0b4f87ef50c4ac9850084f45af97d0986b89d5a
Inju Michorius <inju@hotmail.nl>
2024-09-11 15:03:57 +0200

    Fix script and make website ready for PROD

--- scripts/read-more.js
+++ scripts/read-more.js
@@ -1,5 +1,28 @@
-$(document).ready(function () {
-  $(".expertise-item").click(function () {
-    $(this).toggleClass("active");
+document.addEventListener("DOMContentLoaded", function() {
+  const expertiseBottom = document.querySelectorAll(".expertise-bottom");
+
+  expertiseBottom.forEach(function(section) {
+      const activitiesList = section.querySelectorAll(".activities-list li");
+      const readMoreButton = section.querySelector(".read-more-button");
+
+      // Controleer of de lees meer-knop bestaat
+      if (!readMoreButton) {
+          return; // Sla deze sectie over als er geen knop is
+      }
+
+      // Toon alleen de eerste 4 <li> items
+      activitiesList.forEach(function(item, index) {
+          if (index >= 4) {
+              item.style.display = "none";
+          }
+      });
+
+      // Voeg event listener toe aan de "Lees meer..." knop
+      readMoreButton.addEventListener("click", function() {
+          activitiesList.forEach(function(item) {
+              item.style.display = "list-item";
+          });
+          readMoreButton.style.display = "none"; // Verberg de knop na het klikken
+      });
   });
 });
