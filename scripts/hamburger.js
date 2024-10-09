dff6ba00bb0ed7fe4e93794f4f090509bdc61eda
Inju Michorius <inju@hotmail.nl>
2024-10-09 16:10:16 +0200

    Seperate website into (sub) pages, tweak some navbar behaviour fot better user experience

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -3,5 +3,33 @@ $(document).ready(function () {
     $(this).toggleClass("is-active");
     const menu = document.getElementById("menu");
     $(menu).toggleClass("show");
+
+    // Toggle backdrop and prevent scrolling
+    const backdrop = document.getElementById("backdrop");
+    $(backdrop).toggleClass("show");
+    $("body").toggleClass("no-scroll");
+  });
+
+  // Close menu when a menu item is clicked
+  $(".menu-item").click(function () {
+    $(".hamburger").removeClass("is-active");
+    const menu = document.getElementById("menu");
+    $(menu).removeClass("show");
+
+    // Remove backdrop and allow scrolling
+    const backdrop = document.getElementById("backdrop");
+    $(backdrop).removeClass("show");
+    $("body").removeClass("no-scroll");
+  });
+
+  // Close menu if backdrop is clicked
+  $("#backdrop").click(function () {
+    $(".hamburger").removeClass("is-active");
+    const menu = document.getElementById("menu");
+    $(menu).removeClass("show");
+
+    // Remove backdrop and allow scrolling
+    $(this).removeClass("show");
+    $("body").removeClass("no-scroll");
   });
 });
